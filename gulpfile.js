5e1445afd416eac1ffed45b232a5e317033bd683
and331 <62248319+and331@users.noreply.github.com>
2024-01-07 22:18:52 +0200

    lab2_update

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
 const cleanCSS = require('gulp-clean-css');
+const zip = import('gulp-zip'); 
 
 // Debug: Компіляція Sass у форматований CSS з картами прив'язок
 gulp.task('debug', function () {
@@ -12,7 +13,7 @@ gulp.task('debug', function () {
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest('dist/debug'));
+    .pipe(gulp.dest('dist/'));
 });
 
 // Production: Компіляція та мініфікація Sass у CSS з суфіксом .min
@@ -22,7 +23,15 @@ gulp.task('production', function () {
     .pipe(autoprefixer())
     .pipe(cleanCSS())
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('dist/production'));
+    .pipe(gulp.dest('dist/'));
+});
+
+// Завдання для упаковки файлів в zip архів
+gulp.task('zip', async () => {
+  const gulpZip = (await import('gulp-zip')).default;
+  return gulp.src('dist/**/*')
+    .pipe(gulpZip('archive.zip'))
+    .pipe(gulp.dest('zip/'));
 });
 
 // Спостереження за змінами у файлах Sass для обох конфігурацій
@@ -31,4 +40,6 @@ gulp.task('watch', function () {
 });
 
 // Задача за замовчуванням: запуск усіх задач (Debug та Production)
-gulp.task('default', gulp.parallel('debug', 'production', 'watch'));
+//gulp.task('default', gulp.parallel('debug', 'production', 'watch'));
+gulp.task('prod', gulp.parallel('production', 'zip', 'watch'));
+gulp.task('debug', gulp.parallel('debug', 'zip', 'watch'));
